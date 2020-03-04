2d50946b369901375ba50b75c5cb53ce617ba41c
Abhishek Saral <abhisheksaral17@gmail.com>
2020-03-03 17:01:37 -0800

    - Temporary movie retrieval and display.

--- WatchThis/ViewController.swift
+++ WatchThis/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempMovieLabel: UILabel!
+    
+    
+    
+    var movies = [[String:Any]]()
+    var tempMovieString = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //tempMovieLabel.text = "Hello World!"
+        
+        // Retrieving movie information from The Movie Database API
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/top_rated?api_key=b6dcea27a60a83ccbe00da3c72753438&language=en-US&page=1&region=US")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let movieData = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+            
+            self.movies = movieData["results"] as! [[String:Any]]
+            
+            //print(self.movies)
+
+           }
+        }
+        task.resume()
     }
-
-
+    
+    // Temp button to see if we are getting movies from the database.
+    @IBAction func showMoviesButton(_ sender: Any) {
+        
+        for movie in movies {
+            tempMovieString.append(movie["title"] as! String)
+            tempMovieString.append("\n")
+        }
+        tempMovieLabel.text = tempMovieString
+        print(tempMovieString)
+        
+    }
 }
 
